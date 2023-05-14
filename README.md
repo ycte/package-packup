27ac9b820da3d236fe719793d1acfb98e203e190
ycte <b15260056838@163.com>
2023-05-14 22:18:33 +0800

    add basic server && some problem on thread waiting for solvement

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 * pages: pages
 * store: 数据处理
 * utils: 高复用度工具
+
+# exec
+``` npm install -g nodemon ```
+``` yarn install ```
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const users = require("./routes/users");
+// 服务器代码更改自动重启
+const debug = require('debug')("my-application");
+
+app.use("/api/users", users);
+
+app.listen(3060,() => {
+    debug("server running on port 3060");
+})

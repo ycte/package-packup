5f5cf311d3f789d5a9d48a5f54a161330d348d00
ycte <b15260056838@163.com>
2023-09-25 11:54:14 +0800

    chore(mysql): promise(<pending>)

--- server/src/user.module.ts
+++ server/src/user.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
+  exports: [TypeOrmModule],
 })
 export class UserModule {}
--- server/src/user.service.ts
+++ server/src/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
   getHello(): string {
-    return 'Hello World!';
+    // console.log(this.userRepository.find().resolve());
+    return 'Hello World!' + this.userRepository.find();
   }
   getIndex(): string {
     return `not allowed to login`;

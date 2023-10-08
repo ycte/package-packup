f67fa95a0f9f3f3c170f2cf044bf6f9f7a516d53
ycte <b15260056838@163.com>
2023-10-08 19:38:53 +0800

    feat(form&page): pick-up && state-manage pages-Ahttp

--- src/pages/PickUp/index.js
+++ src/pages/PickUp/index.js
@@ -1,28 +1,29 @@
-import { Input, Card, Button, Modal } from 'antd';
-import { useState } from 'react';
+import { Card, Button } from 'antd';
 import './index.scss'
+import { Ahttp } from '@/utils';
 const PickUp = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-    return (
-      <div>
-        在这里收发行李,有一个表单，里面一个输入框和一个扫码系统
-        <Card>
-          <Input placeholder="输入编号或者扫码" className='noInput'/>
-          <Button onClick={showModal}>扫码</Button>
-          <Modal title="扫码制作中……" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p>这是一个扫码...</p>
-          </Modal>
-        </Card>
-      </div>
-    )
+  const add = async () => {
+    let res = await (Ahttp.post('/pkgstate/add', {
+
+    }))
+    return res
   }
-  export default PickUp
+  // console.log(add())
+  return (
+    <div>
+      <Card>
+        <h1>模式</h1>
+      </Card>
+      <Card>
+        {/* <Button></Button> */}
+        <Button type='primary'
+          onClick={window.location.href = './state-manage'}>
+          工作人员模式
+        </Button>
+        
+      </Card>
+
+    </div>
+  )
+}
+export default PickUp
--- src/pages/StateManage/index.js
+++ src/pages/StateManage/index.js
@@ -1,48 +1,77 @@
-import { Input, Card, Button, Select, Modal } from 'antd';
-import { useState } from 'react';
+import { Ahttp } from '@/utils';
+import { Input, Card, Button, Select, Form } from 'antd';
 const StateManage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
+  const onFinish = async (values) => {
+    console.log('Success:', values);
+    const res = await Ahttp.post('/pkgstate/alter', {
+      pkgId: values.pkgId,
+      state: values.state,
+      reason: '测试一下',
+    })
+    console.log(res);
+    return res;
   };
-  const handleOk = () => {
-    setIsModalOpen(false);
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
   };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };  
   return (
-      <div>
-        在这里工作人员改变行李状态,一个表单,设置状态,手动输入和扫码
-        <Card>
-          <Select 
-            defaultValue="运输中"
-            style={{
-              width: 90,
-            }}
-            options={[
-              {
-                value:"未开始",
-                label:"未开始",
-              },
-              {
-                value:"运输中",
-                label:"运输中",
-              },
-              {
-                value:"已到达",
-                label:"已到达",
-              },
+    <div>
+      在这里工作人员改变行李状态,一个表单,设置状态,手动输入和扫码
+      <Card>
 
+        <Form
+          validateTrigger={['onBlur', 'onChange']}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="pkgId："
+            name="pkgId"
+            rules={[
+              { required: true, message: 'pkgid 不能为空!', },
+              // { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号' },
             ]}
-          />
-          <Input placeholder="输入编号或者扫码" className='noInput'/>
-          <Button onClick={showModal}>扫码</Button>
-          <Modal title="扫码制作中……" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-            <p>这是一个扫码...</p>
-          </Modal>
-        </Card>
-      </div>
-    )
+          >
+            <Input size="large"
+              placeholder="请输入pkgId"
+              defaultValue={'22920202208888-4'} />
+          </Form.Item>
+          <Form.Item
+            label="state："
+            name="state"
+            rules={[
+              { required: true, message: 'state 不能为空!', },
+            ]}
+          >
+            <Select
+              defaultValue="运输中"
+              style={{
+                width: 90,
+              }}
+              options={[
+                {
+                  value: "start",
+                  label: "已装车",
+                },
+                {
+                  value: "goon",
+                  label: "运输中",
+                },
+                {
+                  value: "finish",
+                  label: "已到达",
+                },
+              ]}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              修改
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 export default StateManage
